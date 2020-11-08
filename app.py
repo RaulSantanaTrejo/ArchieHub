ab2f5185965590e2f46a41ce31b7b4d0ced1ab8d
Raul Santana Trejo <r.santanatrejo@student.tudelft.nl>
2020-11-08 18:47:45 +0100

    Added debug prints and references

--- app.py
+++ app.py
@@ -4,7 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    print "Headers: "
+    print request.headers
+
+    print("body: " + request.data )
     return "Hello World!"
+
+
+@app.route('/test')
+def hello():
+    return "You contacted archiehub"
+
+
+@app.route('/memory')
+def hello():
+    return "TODO: reroute to archiememory"
+
 
 if __name__ == '__main__':
     app.run()
